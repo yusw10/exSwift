c1b663ee83fefbd7ba7c26b938aa2574c95fd282
HANSUK <yusw10@naver.com>
2022-02-21 18:26:28 +0900

    setting swift package manager of Netflix Style app 220221

---
+++ Example/NetflixStyleSampleApp/NetflixStyleSampleApp/HomeViewController.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewController.swift
+//  NetflixStyleSampleApp
+//
+//  Created by 유한석 on 2022/02/21.
+//
+
+import UIKit
+
+class HomeViewController: UICollectionViewController{
+    
+}
