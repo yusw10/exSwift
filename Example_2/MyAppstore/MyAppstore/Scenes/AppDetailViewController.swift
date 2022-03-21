384fd4e136fe1693c20a2404d509970ae3158f32
HANSUK <yusw10@naver.com>
2022-03-21 17:46:06 +0900

    complete appstore app 220321

--- Example_2/MyAppstore/MyAppstore/Scenes/AppDetailViewController.swift
+++ Example_2/MyAppstore/MyAppstore/Scenes/AppDetailViewController.swift
@@ -21,6 +21,64 @@ final class AppDetailViewController: UIViewController {
     private var downloadButton = UIButton().then{make in
         make.setTitle("닫기", for: .normal)
         make.titleLabel?.font = .systemFont(ofSize: 13.0, weight: .bold)
+        make.backgroundColor = .systemBlue
+        make.setTitleColor(.white, for: .normal)
+        make.layer.cornerRadius = 12.0
     }
-    
+    private var shareButton = UIButton().then{ make in
+        make.setImage(UIImage(systemName: "square.and.arrow.up"), for: .normal)
+        make.tintColor = .systemBlue
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        setupViews()
+        
+        appIconImageView.backgroundColor = .lightGray
+        titleLabel.text = "타이틀"
+        subTitleLabel.text = "asdasdasdasdasd"
+    }
+}
+//MARK: Private
+private extension AppDetailViewController{
+    func setupViews(){
+        [appIconImageView,
+         titleLabel,
+         subTitleLabel,
+         downloadButton,
+         shareButton
+        ].forEach { UIView in
+            view.addSubview(UIView)
+        }
+        
+        appIconImageView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(16.0)
+            make.top.equalToSuperview().inset(32.0)
+            make.height.equalTo(100.0)
+            make.width.equalTo(appIconImageView.snp.height)
+        }
+        titleLabel.snp.makeConstraints { make in
+            make.leading.equalTo(appIconImageView.snp.trailing).offset(16.0)
+            make.trailing.equalToSuperview().inset(16.0)
+            make.top.equalToSuperview().inset(32.0)
+        }
+        subTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(4.0)
+            make.leading.equalTo(titleLabel.snp.leading)
+            make.trailing.equalToSuperview().inset(16.0)
+        }
+        downloadButton.snp.makeConstraints { make in
+            make.width.equalTo(60.0)
+            make.height.equalTo(32.0)
+            make.leading.equalTo(appIconImageView.snp.trailing).offset(16.0)
+            make.bottom.equalTo(appIconImageView.snp.bottom)
+        }
+        shareButton.snp.makeConstraints { make in
+            make.width.height.equalTo(32.0)
+            make.trailing.equalToSuperview().inset(16.0)
+            make.bottom.equalTo(appIconImageView.snp.bottom)
+        }
+        
+        
+    }
 }
