f431492162525a0c03275e0a153cdd557ed43a82
HANSUK <yusw10@naver.com>
2022-04-02 10:36:15 +0900

    220402

---
+++ hs_ex1/SwiftPractice/SwiftPractice/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  SwiftPractice
+//
+//  Created by 유한석 on 2022/03/30.
+//
+
+import Foundation
+
+let inputNM = readLine()!.split(separator: " ")
+let N = Int(inputNM[0])!
+let M = Int(inputNM[1])!
+var map = [[Int]]()
+
+for 
